d329cfa0b94c6930f201a90cd1d77ffe5d88eb69
JoseBorbon <joserborbon@gmail.com>
2021-10-02 22:25:17 -0400

    changed botJS file to be dynamic with commands

--- bot.js
+++ bot.js
@@ -1,27 +1,44 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 dotenv.config();
 // Require the necessary discord.js classes
-const { Client, Intents } = require("discord.js");
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
 const token = process.env.BOT_TOKEN;
 // Create a new client instance
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// When the client is ready, run this code (only once)
-client.once("ready", () => {
-  console.log("Ready! logged in as " + client.user.tag);
+client.commands = new Collection();
+//Used to get all command files in directory that are a js extension
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+commandFiles.forEach((file) => {
+  const command = require(`./commands/${file}`);
+  //Sets a new item in the Collection (Map) with the command name as key and value as exported module
+  client.commands.set(command.data.name, command);
 });
 
-client.on("interactionCreate", async (interaction) => {
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+  console.log('Ready! logged in as ' + client.user.tag);
+});
+
+client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName } = interaction;
+  const command = client.commands.get(interaction.commandName);
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply("Server info.");
-  } else if (commandName === "user") {
-    await interaction.reply("User info.");
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
   }
 });
 

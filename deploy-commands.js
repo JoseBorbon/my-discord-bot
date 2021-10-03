c220a2f69b326512034a95517c403d6244ec70a4
JoseBorbon <joserborbon@gmail.com>
2021-10-02 22:25:41 -0400

    made deploy-commandsJS dynamic for commands

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,26 @@
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
+const fs = require('fs');
 dotenv.config();
 const token = process.env.BOT_TOKEN;
-console.log(token);
-const { SlashCommandBuilder } = require("@discordjs/builders");
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const { clientId, guildId } = require("./config.json");
 
-const commands = [new SlashCommandBuilder().setName("ping").setDescription("Replies with pong!"), new SlashCommandBuilder().setName("server").setDescription("Replies with server info!"), new SlashCommandBuilder().setName("user").setDescription("Replies with user info!")].map((command) =>
-  command.toJSON()
-);
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
 
-const rest = new REST({ version: "9" }).setToken(token);
+const commands = [];
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+commandFiles.forEach((file) => {
+  //Used to get current command we will be pushing to commands
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
+});
+
+const rest = new REST({ version: '9' }).setToken(token);
 
 rest
   .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-  .then(() => console.log("Successfully registered application commands."))
+  .then(() => console.log('Successfully registered application commands.'))
   .catch(console.error);
